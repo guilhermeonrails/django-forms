bb9a4d6adde17a11b52e7ec50273b89a9ddd1bf3
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-02-05 15:55:32 -0300

    exibindo meu nome

--- passagens/urls.py
+++ passagens/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('meu_nome', views.meu_nome, name='meu_nome')
 ]

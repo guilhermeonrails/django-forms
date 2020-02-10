cf3b7f282d5a7aabb9918836374253cec98d0ded
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-02-10 14:51:28 -0300

    add widget-tweaks

--- passagens/forms.py
+++ passagens/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from datetime import datetime
 
 class PassagemForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
+    origem = forms.CharField(label='Origem', max_length=100, )
     destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida')
+    data_ida = forms.DateField(label='Ida', )
     data_volta = forms.DateField(label='Volta')
     data_pesquisa = forms.DateField(disabled=True,initial=datetime.today)
     informacoes = forms.CharField(
--- passagens/views.py
+++ passagens/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .forms import PassagemForms
 
 def index(request):

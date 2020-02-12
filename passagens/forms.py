dc387c704a2383ec243b6386566cc5b0bde8b649
Guilherme Lima <guilhermelimaruby@gmail.com>
2020-02-12 13:11:51 -0300

    incluindo date picker

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from datetime import datetime
-from django.contrib.admin.widgets import AdminDateWidget
+from tempus_dominus.widgets import DatePicker
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100, )
     destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida', widget=forms.SelectDateWidget(), initial=datetime.today)
-    data_volta = forms.DateField(label='Volta', widget=forms.SelectDateWidget(), initial=datetime.today)
-    data_pesquisa = forms.DateField(disabled=True,initial=datetime.today)
+    data_ida = forms.DateField(label='Ida',widget=DatePicker())
+    data_volta = forms.DateField(label='Volta',widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today)
     informacoes = forms.CharField(
         label='Informações extras',
         max_length=200,
@@ -32,10 +34,13 @@ class PassagemForms(forms.Form):
     def clean(self):
         origem = self.cleaned_data['origem']
         destino = self.cleaned_data['destino']
+        data_pesquisa = self.cleaned_data['data_pesquisa']
         data_ida = self.cleaned_data['data_ida']
         data_volta = self.cleaned_data['data_volta']
         if origem == destino:
             self.add_error('destino','O campo destino não pode ser igual ao campo origem')
+        if data_ida < data_pesquisa:
+            self.add_error('data_ida','Data da ida inválida')
         if data_ida > data_volta:
             self.add_error('data_ida','Data da ida não pode ser maior que data da volta')
         return  self.cleaned_data
--- setup/settings.py
+++ setup/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tempus_dominus',
     'widget_tweaks',
     'passagens',
     'django.contrib.admin',
@@ -115,8 +116,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPUS_DOMINUS_LOCALIZE = True
